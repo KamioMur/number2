448694156d0ee0fd3518b6332f6f3014c78508ae
KamioMur <seba.mihaelis@yandex.ru>
2018-06-18 00:18:25 +0300

    ‘Last’

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         System.out.println("Hello World!");
         System.out.println("Second commit");
-        System.out.println("Next Commit");
+        System.out.println("The End");
 
     }
 }
